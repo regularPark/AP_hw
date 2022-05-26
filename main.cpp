5a61b4a5ae365cff1e1e0403f2c2596a0314b761
박정규 <srk99279@naver.com>
2022-05-26 22:42:55 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ using namespace std;
 #include "subject.h"
 
 
-
 int main() {
 	ifstream fin;
 	int sem;
@@ -89,6 +88,7 @@ int main() {
 	int select;
 	string name;
 	int n;
+	string sub_name;
 
 	while (true) {
 		cout << "\n\n----------하고자 하는 작업을 선택----------" << endl;
@@ -97,22 +97,38 @@ int main() {
 		cout << "3. 전체 등수 출력" << endl;
 		cout << "4. 학기별 등수 출력" << endl;
 		cout << "5. 학생 1명의 개별 과목 성적, 학기 평점, 총 평점 출력" << endl;
+		cout << "6. 종료" << endl;
 		cout << endl;
 		cout << "실행하고자 하는 기능의 번호 입력(1 ~ 5 사이의 정수) > ";
 		cin >> select;
 
 		switch (select) {
 		case 1:
+			// 예외 : 이름이 없을 떄, 동명의 학생이 있을때-> 학번으로 검색하도록 유도. 전체 평점 같으면, 전공 평점 높은 사람 등수가 높음.
 			cout << "성적을 확인하고자 하는 학생의 이름 입력 > ";
 			cin >> name;
 			mme.searchStd(name);
 			break;
 		case 2:
-			cout << "n을 입력 > ";
+			// 예외 처리 : 과목 없을 때, 등수가 0~15 사이의 정수가 아닐때 
+			cout << "과목 이름을 입력 > ";
+			cin >> sub_name;
+			cout << "알고 싶은 등수를 입력 > ";
 			cin >> n;
+			mme.searchNrank(sub_name, n);
 			break;
+		case 3:
+			cout << "-----------전체 등수-----------";
+			break;
+		case 4:
+			cout << "----------학기별 등수----------";
+			break;
+		case 5:
+
+		case 6:
+			
 		default:
-			cout << "1 ~ 5 사이의 정수를 입력해주세요" << endl;
+			cout << "1 ~ 6 사이의 정수를 입력해주세요" << endl;
 			break;
 		}
 	}
