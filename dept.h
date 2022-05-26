a537cc01f490cb64a2e99d8b179e974c36145a2e
박정규 <srk99279@naver.com>
2022-05-27 02:04:41 +0900

    Update dept.h

--- dept.h
+++ dept.h
@@ -9,7 +9,6 @@ using namespace std;
 #include "student.h"
 #include "subject.h"
 
-
 class Dept {
 	vector<Student> std_list;
 
@@ -26,7 +25,10 @@ public:
 	void getSubRank_1p(int s_id);
 	void searchNrank(string name, int n);
 	void printSubRank(map<int, float> rank_arr, int n);
+	void showSemRank();
+	void getTotalRank(map<int, float> gpa_1, map<int, float> gpa_2, map<int, float> m_gpa_1, map<int, float> m_gpa_2);
 
-	void printGPA(vector<float> rank);
+
+	void printGPA(map<int, float> rank, map<int, float> mRank);
 	void printGPA_1p(map<int, float> rank, map<int, float> mRank, int s_id);
 };
