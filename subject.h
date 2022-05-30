ee69d47567ee83123a4fabeda2339395a826cd88
박정규 <srk99279@naver.com>
2022-05-30 14:30:34 +0900

    Update subject.h

--- subject.h
+++ subject.h
@@ -2,20 +2,27 @@
 #include <string>
 using namespace std;
 
+
+
 class Subject {	// 과목 클래스에는 과목명과 점수가 입력됨.
 	string name;
 	int score;
 	bool isMajor;
 	float grade;
+	string GPA;
 public:
 
 	Subject();
 	~Subject();
 
-	void setSub(string name, int score, bool isMajor, float grade);
+	void setSub(string name, int score, bool isMajor, float grade, string GPA);
 	bool isMajorChk(string name);
 	int getScore() { return this->score; }
+	string getSubName() { return this->name; }
 	float getGrade() { return this->grade; }
 	bool isMjr() { return this->isMajor; }
+	string getGPA() { return this->GPA; }
 	float setGrade(int score);
+
+	string setGPAgrade(int score);
 };
