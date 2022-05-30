ac8bf23af2bfeba88148e3a7d0203684bf6f6b27
박정규 <srk99279@naver.com>
2022-05-30 14:31:01 +0900

    Update subject.cpp

--- subject.cpp
+++ subject.cpp
@@ -3,11 +3,12 @@
 Subject::Subject() { this->name = "Nothing"; }
 Subject::~Subject() {};
 
-void Subject::setSub(string name, int score, bool isMajor, float grade) {
+void Subject::setSub(string name, int score, bool isMajor, float grade, string GPA) {
 	this->name = name;
 	this->score = score;
 	this->isMajor = isMajor;
 	this->grade = grade;
+	this->GPA = GPA;
 }
 bool Subject::isMajorChk(string name) {
 	if (name[name.length() - 1] == 'm') {
@@ -43,5 +44,36 @@ float Subject::setGrade(int score) {
 	}
 	else if (score > 0 && score <= 60) {
 		return 0.0;
+	}
+}
+
+
+string Subject::setGPAgrade(int score) {
+	if (score > 95 && score <= 100) {
+		return "A+";
+	}
+	else if (score > 90 && score <= 95) {
+		return "A0";
+	}
+	else if (score > 85 && score <= 90) {
+		return "B+";
+	}
+	else if (score > 80 && score <= 85) {
+		return "B0";
+	}
+	else if (score > 75 && score <= 80) {
+		return "C+";
+	}
+	else if (score > 70 && score <= 75) {
+		return "C0";
+	}
+	else if (score > 65 && score <= 70) {
+		return "D+";
+	}
+	else if (score > 60 && score <= 65) {
+		return "D0";
+	}
+	else if (score > 0 && score <= 60) {
+		return "F";
 	}
 }
