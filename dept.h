ab05a91f30a525dda1f5464220fc78123b815fce
박정규 <srk99279@naver.com>
2022-05-30 14:30:10 +0900

    Update dept.h

--- dept.h
+++ dept.h
@@ -13,22 +13,26 @@ class Dept {
 	vector<Student> std_list;
 
 public:
-
+	
 	Dept();
 	~Dept();
 
+
+
 	void setStd(Subject sub, char(*sub_arr)[10], char(*info_arr)[10], int i);
-	void searchStd(string name);
-	void searchStd(int id);
+	int searchStd(string name);
+	void searchSid(int s_id);
+
 	map<int, float> getGPA(int s_id);
 	map<int, float> getMajorGPA(int s_id);
-	void getSubRank_1p(int s_id);
-	void searchNrank(string name, int n);
+	void searchNrank();
 	void printSubRank(map<int, float> rank_arr, int n);
 	void showSemRank();
 	void getTotalRank(map<int, float> gpa_1, map<int, float> gpa_2, map<int, float> m_gpa_1, map<int, float> m_gpa_2);
-
+	void getTotalRank_1p(map<int, float> gpa_1, map<int, float> gpa_2, map<int, float> m_gpa_1, map<int, float> m_gpa_2, int s_id);
 
 	void printGPA(map<int, float> rank, map<int, float> mRank);
 	void printGPA_1p(map<int, float> rank, map<int, float> mRank, int s_id);
+	void printSubName();
+	void printGPA_(int s_id);
 };
